880b5afe4932457f734a1d8408e38bef5f0c5f02
melvin <m9812011@gmail.com>
2018-04-12 21:11:38 +0800

    cordova-plugin-network官網是使用javascript作為範例，試不出來。因此改用ionic官網(typescript) package.json 添加 => "@ionic-native/network": "^4.7.0",

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Network } from '@ionic-native/network';
+
+import {} from ''
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public network: Network) {
 
   }
 
