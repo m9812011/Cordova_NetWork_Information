3e36ada59ee95d9debaf7e7f299de3e443426261
melvin <m9812011@gmail.com>
2018-04-13 15:44:50 +0800

    偵測網路狀態 --

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController , ToastController} from 'ionic-angular';
 import { Network } from '@ionic-native/network';
 
-import {} from ''
 
 @Component({
   selector: 'page-home',
@@ -11,12 +9,34 @@ import {} from ''
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public network: Network) {
-
+  constructor(private toast: ToastController, private network: Network, public navCtrl: NavController) {
+    console.log("constructor");
   }
 
-  public checkNetworkState(): void{
-    console.log("checkNetworkState()");
+  // Runs when the page has loaded.
+  ionViewDidEnter(){
+    this.network.onConnect().subscribe(data => {
+      console.log(data)
+      this.displayNetworkUpdate(data.type);
+    }, error => console.error(error));
+
+    this.network.onDisconnect().subscribe(data => {
+      this.displayNetworkUpdate(data.type);
+      console.log(data)
+    }, error => console.error(error));
   }
+
+  public displayNetworkUpdate(connectionState: string){
+    let networkType = this.network.type;
+    this.toast.create({
+      message: `You are now ${connectionState} via ${networkType}`,
+      duration: 3000
+    }).present();
+  }
+
+  // public checkNetworkState(): void{
+  //   console.log("checkNetworkState()");
+
+  // }
 
 }
